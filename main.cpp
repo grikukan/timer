6619eb80a5854aefd433077c570fb01337a6d8d4
grikukan <grikukan@mail.ru>
2014-10-12 17:51:46 +0400

    Cmake -> qmake

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+#include <clocale>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int time;
+    cin>>time;
+    while(time)
+    {
+        cout<<"Осталось чихать "<<time<<" раз(-а)"<<endl;
+        time--;
+        Sleep(1000);
+    }
+}
